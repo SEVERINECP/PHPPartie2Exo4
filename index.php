d5158d05f7dc522c63a64cfe7f2e1f90d3e5fd8e
SEVERINECP <sevpicot@gmail.com>
2019-12-04 14:09:48 +0100

    PHPPartie2Exo4End

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$age = 18;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHPPartie2Exo4</title>
+  </head>
+  <body>
+    <?php if ($age >= 18){ ?>
+      Tu es majeur
+    <?php }else{ ?>
+      Tu es mineur
+    <?php } ?>  
+  </body>
+</html>
